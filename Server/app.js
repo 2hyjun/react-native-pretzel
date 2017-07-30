bc0ff78292f9e56b7850fcc14dd2460df4629cfd
HyunahKwon <hakwon9439@gmail.com>
2017-07-31 00:51:05 +0900

    Revert "Server 0727 Chat Webpage ver."
    This reverts commit 37a80608f9e10dd66c07c349e6a2970ff4d0580f.

--- Server/app.js
+++ Server/app.js
@@ -9,7 +9,6 @@ var io = require('socket.io').listen(server);
 
 var session = require('express-session');
 var MYSQLStore = require('express-mysql-session')(session);
-var _ = require('underscore');
 
 var mysql = require('mysql'),
     crypto = require('crypto'),
@@ -23,25 +22,20 @@ var mysqlSessionOptions = {
     // mysql 접속 정보.
     // session에서 만 사용.
 }
-var mysqlSession = session({
+app.use(session({
     secret: 'kGusbVJHUSGD6$VgAS4S^VGB',
     resave: false,
     saveUninitialized: true,
     store: new MYSQLStore(mysqlSessionOptions)
-});
-app.use(mysqlSession);
-
-var sharedSession = require('express-socket.io-session');
-io.use(sharedSession(mysqlSession, {
-    autosave: true
 }))
+
 app.set('view engine', 'pug');
 app.locals.pretty = true;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-var signedUser;
+
 var homepage = require('./routes/hompage');
 var logout = require('./routes/logout.js');
 var login = require('./routes/login');
@@ -51,17 +45,7 @@ var new_request = require('./routes/newRequest');
 var timeline_delete = require('./routes/timeline_delete');
 var timeline_update = require('./routes/timeline_update');
 var mypage = require('./routes/mypage')
-app.get('/', (req, res) => {
-    if (req.session.signedUser) {
-        res.send({
-            resultCode: 100
-        })
-    } else {
-        res.send({
-            resultCode: 1
-        })
-    }
-});
+app.get('/', homepage);
 app.get('/logout', logout);
 app.post('/login', login);
 app.post('/register', register);
@@ -71,55 +55,22 @@ app.get("/timeline/delete/:id", timeline_delete);
 app.post('/timeline/update/:id', timeline_update);
 app.get('/mypage', mypage);
 
-app.get('/login', (req, res) => {
-    res.render('new_request');
+app.get('/chat', (req, res) => {
+    res.sendfile('./chat.html');
 })
-var onlineUser = [];
-var clients = 0;
 
-
+var onlineUser = {};
 io.on('connection', (socket) => {
     clients++;
-    var email = socket.handshake.session.signedUser.user_email;
-    if (email) {
-        var isInList = _.find(onlineUser, (user) => {
-            return user.email == email
-        })
-        if (isInList) {
-            var index = _.findIndex(onlineUser, (user) => {
-                return user.email == email
-            })
-            onlineUser[index].id = socket.id;
-            socket.emit('conn', 'Re Hello, ' + socket.handshake.session.signedUser.user_email)
-        } else {
-            var user = {};
-            user['email'] = email;
-            user['id'] = socket.id;
-            onlineUser.push(user);
-            socket.emit('conn', 'Hello, ' + socket.handshake.session.signedUser.user_email)
-        }
-        io.to(socket.id).emit('message', {
-            from: 'admin',
-            message: 'Hi guy'
-        })
-
-    } else {
-        socket.emit('conn', 'Please log in First');
-    }
-    socket.on('message', (data) => {
-        var index = _.findIndex(onlineUser, (user) => {
-            return user.email == data.dest
-        })
-        io.to(onlineUser[index].id).emit('message', data);
+    socket.emit('Hello!');
+    socket.broadcast.emit('new', clients + ' clients connected!\nYour Socket ID: ' + socket.id)
+    socket.on('disconnect', () => {
+        clients--;
+        socket.broadcast.emit('new', clients + ' clients connected!\nYour Socket ID: ' + socket.id)
     })
+    socket.send(socket.id)
 })
 
-app.get('/chat', (req, res) => {
-    res.render('chat', {
-        list: onlineUser,
-        user: req.session.signedUser.user_email,
-    })
-})
 server.listen(port, () => {
     console.log('Pretzel Server listening at port', port);
 })
