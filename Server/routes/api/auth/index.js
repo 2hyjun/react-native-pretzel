1db756530a8fe25f6bd7904f6ad8c4d41f2cbbb8
2hyjun <biper94@gmail.com>
2017-08-25 22:27:16 +0900

    0825 Server

--- Server/routes/api/auth/index.js
+++ Server/routes/api/auth/index.js
@@ -7,8 +7,10 @@ router.use('/check', authMiddleware);
 
 router.post('/register', controller2.register);
 router.post('/login', controller.login);
-router.get('/checkEmailAuth', controller2.checkEmailAuth);
 
+router.get('/checkEmailAuth', controller2.checkEmailAuth);
 router.get('/check', controller.check);
 router.get('/emailAuthPage', controller.emailAuthPage);
+router.get('/emailVerified', controller2.emailVerified);
+
 module.exports = router;
